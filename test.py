5d15f8474f4ed1e7fcc51e052635cc9424eaee51
Gabriel <gacorderocc@gmail.com>
2023-07-03 18:59:20 -0500

    Comienzo el proyecto con el archivo de consola

---
+++ test.py
@@ -0,0 +1,41 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: división por cero"
+
+print("Calculadora básica")
+print("Operaciones disponibles:")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicación")
+print("4. División")
+
+operacion = input("Selecciona una operación (1/2/3/4): ")
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+if operacion == "1":
+    resultado = suma(num1, num2)
+    print("El resultado de la suma es:", resultado)
+elif operacion == "2":
+    resultado = resta(num1, num2)
+    print("El resultado de la resta es:", resultado)
+elif operacion == "3":
+    resultado = multiplicacion(num1, num2)
+    print("El resultado de la multiplicación es:", resultado)
+elif operacion == "4":
+    resultado = division(num1, num2)
+    print("El resultado de la división es:", resultado)
+else:
+    print("Operación inválida. Por favor, selecciona una opción válida del 1 al 4 (1/2/3/4).")
